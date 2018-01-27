a9f364ce38312d342fa986d0c990ddaef46aad8d
Unknown <M.john.orfanos@gmail.com>
2018-01-27 18:49:56 -0500

    Added Timer

---
+++ Assets/_Markos/Scripts/ButtonManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour {
+    public GameObject Button1;
+    public GameObject PuzzleManager;
+    public void PressButton() {
+    }
+    
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
