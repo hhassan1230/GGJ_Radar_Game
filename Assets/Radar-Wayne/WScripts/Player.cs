a919407490a71623d0d4efb1c4c31ad47fd989e9
wvj1992 <wvj1992@gmail.com>
2018-01-27 16:53:57 -0500

    Added my folder with Radar Assets and some code. Still working on some code.

---
+++ Assets/Radar-Wayne/WScripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Player : MonoBehaviour {
+
+	private void Awake() {
+		RadarObserver.colliders.Add (gameObject, GetComponent<Collider2D> ());
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
