ae23e2fd6bb34126662f5eeea08bda46d8f42e23
wvj1992 <wvj1992@gmail.com>
2018-01-28 08:03:12 -0500

    Phone Device Orientation works.

---
+++ Assets/Radar-Wayne/WScripts/OrientationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrientationTest : MonoBehaviour {
+
+	public Text textbox;
+
+	// Use this for initialization
+	void Start () {
+		textbox.text = "Device Orientation: FaceUp";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.deviceOrientation == DeviceOrientation.FaceDown) {
+			textbox.text = "Device Orientation: FaceDown";
+		} else if (Input.deviceOrientation == DeviceOrientation.FaceUp) {
+			textbox.text = "Device Orientation: FaceUp";
+		}
+	}
+}
