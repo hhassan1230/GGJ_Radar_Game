7122992ce2372d9fe0ed0e2d5a14532a7684cd67
hhassan1230 <hhassan@oswego.edu>
2018-01-28 11:25:08 -0500

    adds script to center radar

---
+++ Assets/SetRadar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetRadar : MonoBehaviour {
+	private GameObject radarScan;
+	private GameObject radar;
+	// Use this for initialization
+	void Start () {
+		Vector2 position = new Vector2 (-354.87f, -391.37f);
+		radarScan = GameObject.FindGameObjectWithTag ("radar-scan");
+		radar = GameObject.FindGameObjectWithTag ("radar");
+		radar.transform.localPosition = position;
+		radarScan.transform.localPosition = position;
+	}
+
+}
