1d54f8025dfd2427684a85e6ac0a849f3350ed31
hhassan1230 <hhassan@oswego.edu>
2018-01-31 10:35:19 -0500

    atpmt to make tutoral page

---
+++ Assets/RotateRadar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateRadar : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void rotateObject(){
+		transform.Rotate(Vector3.forward * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rotateObject ();
+	}
+}
---
+++ Assets/TutoralRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoralRotate : MonoBehaviour {
+	public float speed = -90f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OrbitAround() {
+		transform.RotateAround (Vector3.zero, Vector3.down, speed * Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// OrbitAround ();
+		transform.Rotate(Vector3.forward * speed);
+	}
+}
